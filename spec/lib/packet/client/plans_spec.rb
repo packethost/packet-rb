4c9810780a0085162c40c6d1d458803a29b49ac5
Lucas Perez <lucasmpb@gmail.com>
2015-06-16 17:06:06 -0300

    Simplify plan stub request headers

--- spec/lib/packet/client/plans_spec.rb
+++ spec/lib/packet/client/plans_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Packet::Client::Plans do
     end
 
     stub_request(:get, 'https://api.packet.net/plans')
-      .with(headers: { 'Accept' => 'application/json', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type' => 'application/json', 'User-Agent' => 'Faraday v0.9.1', 'X-Auth-Token' => 'auth_token' })
+      .with(headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json', 'X-Auth-Token' => 'auth_token' })
       .to_return(status: 200, body: '{"plans":[{"id":"6d1f1ffa-7912-4b78-b50d-88cc7c8ab40f","slug":"baremetal_1","name":"Type 1","description":"Our Type 1 configuration is a zippy general use server, with an Intel E3-1240 v3 processor and 16GB of RAM.","line":"baremetal","specs":{"cpus":[{"count":1,"type":"Intel E3-1240 v3"}],"memory":{"total":"16GB"},"drives":[{"count":2,"size":"120GB","type":"SSD"}],"nics":[{"count":2,"type":"gigabit"}],"features":{"raid":true,"txt":true}},"pricing":{"hourly":0.4}}]}', headers: { 'Content-Type' => 'application/json; charset=utf-8' })
   end
 
